789d8e185c516ca49f9d713b1b58a42887d266ab
Vrganj <43708436+Vrganj@users.noreply.github.com>
2025-10-30 01:43:20 +0100

    add missing files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,39 @@
 plugins {
     id("java")
+    id("de.eldoria.plugin-yml.bukkit") version "0.8.0"
 }
 
 group = "me.vrganj"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+bukkit {
+    name = "PufferPanelConsoleTest"
+    version = "1.0"
+    description = "This is a test plugin for the PufferPanel console"
+
+    main = "me.vrganj.pufferpanelconsoletest.PufferPanelConsoleTestPlugin"
+
+    apiVersion = "1.21"
+    author = "Vrganj"
+
+    commands {
+        register("pufferpanelconsoletest") {
+            description = "This is a test command!"
+        }
+    }
 }
--- src/main/java/me/vrganj/pufferpanelconsoletest/PufferPanelConsoleTestPlugin.java
+++ src/main/java/me/vrganj/pufferpanelconsoletest/PufferPanelConsoleTestPlugin.java
@@ -1,4 +1,29 @@
 package me.vrganj.pufferpanelconsoletest;
 
-public class PufferPanelConsoleTestPlugin {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public class PufferPanelConsoleTestPlugin extends JavaPlugin implements CommandExecutor {
+
+    @Override
+    public void onEnable() {
+        // Register the command executor for the "pufferpanelconsoletest" command
+        this.getCommand("pufferpanelconsoletest").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("TEST " + i);
+            } else {
+                System.err.println("TEST " + i);
+            }
+        }
+
+        return true;
+    }
 }
